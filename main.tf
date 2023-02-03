007afbe721d26ac7dbddf829a7ee21a1cc0fa56f
Jay Lavine <jazzlyj@users.noreply.github.com>
2023-02-02 21:08:24 -0800

    chnage sg name

--- main.tf
+++ main.tf
@@ -47,7 +47,7 @@ resource "aws_instance" "blog" {
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.17.1"
-  name    = "blog_new"
+  name    = "blog"
 
   vpc_id  = module.vpc.public_subnets[0]
 
